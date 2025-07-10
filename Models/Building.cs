903cdeacf05f06c2a21706c0ce06c57576b314c2
philopateer25 <philopateerhanyk360@gmail.com>
2025-07-10 13:13:30 +0300

    edit building name

--- Models/Building.cs
+++ Models/Building.cs
@@ -3,7 +3,7 @@
     public class Building
     {
         public int BuildId { get; set; }
-        public string BuildName { get; set; }
+        public string Name { get; set; }
         public string BuildAddress { get; set; }
     }
 }
