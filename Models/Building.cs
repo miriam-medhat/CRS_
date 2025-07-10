5a7a93997accc1d69ff55e2d6181941fcb0664c5
philopateer25 <philopateerhanyk360@gmail.com>
2025-07-10 13:10:23 +0300

    building added

--- Models/Building.cs
+++ Models/Building.cs
@@ -2,5 +2,8 @@
 {
     public class Building
     {
+        public int BuildId { get; set; }
+        public string BuildName { get; set; }
+        public string BuildAddress { get; set; }
     }
 }
